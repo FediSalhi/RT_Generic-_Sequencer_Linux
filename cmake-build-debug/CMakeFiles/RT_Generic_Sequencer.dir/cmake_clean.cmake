9ca5c04e7014b87f1da2c8f89c03113339479525
fedisalhi <fadi.salhi@outlook.fr>
2021-11-05 16:30:02 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/RT_Generic_Sequencer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RT_Generic_Sequencer.dir/main.c.o"
+  "RT_Generic_Sequencer.pdb"
+  "RT_Generic_Sequencer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RT_Generic_Sequencer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
